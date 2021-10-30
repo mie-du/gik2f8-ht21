939620b0c223d52e44daa4af9fa733fc55b00889
Mikaela Hedberg <mikaela.hedberg@plandigital.se>
2021-10-30 11:15:58 +0200

    create start

--- js/User.js
+++ js/User.js
@@ -28,3 +28,14 @@ function renderUser(user) {
   </li>
 `;
 }
+
+function userCreate(event) {
+  event.preventDefault();
+  let form = document.querySelector('.create__form');
+  const data = {
+    f_name: form.f_name.value,
+    l_name: form.l_name.value,
+    email: form.email.value,
+  };
+  console.log(data);
+}
