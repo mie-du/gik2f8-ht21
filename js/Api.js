5766402209ba0fe9ef70e54416e1f6b99e1daad6
Mikaela Hedberg <mikaela.hedberg@plandigital.se>
2021-10-30 11:54:03 +0200

    simplified first step

--- js/Api.js
+++ js/Api.js
@@ -4,31 +4,14 @@
 */
 const url = 'https://rest-small.herokuapp.com/users';
 
-//get object by ID
-function get(id) {
-  return fetch(`${url}/${id}`);
-}
-
 //Get All, converted to JSON for cleanest possible result to work with
 function getAllJSON() {
   //first response returns data stream, needs to be converted to json.
-  const result = fetch(url)
+  fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      return data;
+      console.log(data);
     });
+}
 
-  return result;
-}
-//create
-function create(data) {
-  return fetch(`${url}/${id}`, { method: 'POST' });
-}
-//update
-function update(data) {
-  return fetch(`${url}/${id}`, { method: 'PUT' });
-}
-//delete
-function remove(data) {
-  return fetch(`${url}/${id}`, { method: 'DELETE' });
-}
+getAllJSON();
