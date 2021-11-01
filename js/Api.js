f0836bd8556e588ef03d9f6ceac750af98f28f5a
Mikaela Hedberg <mie@du.se>
2021-11-01 11:14:27 +0100

    merge error

--- js/Api.js
+++ js/Api.js
@@ -2,76 +2,24 @@
   Code for general CRUD-operations with promises.
   Generally written (not necesarily users)
 */
-//https://rest-small.herokuapp.com/docs/
-//https://rest-large.herokuapp.com/docs/
-const url = 'https://rest-small.herokuapp.com/users';
+const url = 'https://rest-small.herokuapp.com/users/';
 
 //get object by ID
-function getById(id) {
-  return fetch(`${url}/${id}`);
+function get(id) {
+  return fetch(`${url}${id}`);
 }
 
 //Get All, converted to JSON for cleanest possible result to work with
 function getAllJSON() {
-  const result = fetch(url).then((response) => response.json());
-  return result;
-}
-
-//returns promise - w/o stopping execution - ES2017
-async function getAllAsync() {
-  //inside async function we can wait for promises
-  const result = await fetch(url);
-  const data = await result.json();
-  console.log('Get All async', data);
-  return data;
-}
-
-/* Examples */
-//chain of async AJAX-calls.
-function getChainFetch() {
+  //first response returns data stream, needs to be converted to json.
   const result = fetch(url)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      return getById(data[0].id);
-    })
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-    });
-
-  return result;
-}
-
-//chain of async ajax-calls with arrow-functions
-function getChainFetchArrow() {
-  const result = fetch(url)
-    .then((response) => response.json())
-    .then((data) => getById(data[0].id))
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      return data;
     });
 
   return result;
 }
-
-//chain of async ajax calls with async/await for consuming promises. Must be more than 1 user.
-async function getChainAsync() {
-  try {
-    const allUsers = await fetch(url);
-    const allUsersJson = await allUsers.json();
-    const user = await getById(allUsersJson[1].id);
-    const userJson = await user.json();
-    return userJson;
-  } catch (err) {
-    throw err;
-  }
-}
-
 //create
 function create(data) {
   console.log(JSON.stringify(data));
@@ -92,13 +40,3 @@ function update(data) {
 function remove(data) {
   return fetch(`${url}/${id}`, { method: 'DELETE' });
 }
-//async function will run when it's done. First and third will be written out first, even though the function call is in the middle.
-console.log('first');
-const returnedValue = getChainAsync();
-console.log('third');
-console.log(returnedValue);
-
-getChainAsync().then(
-  (result) => console.log(result),
-  (err) => console.log(`Oh no! ${err.message}`)
-);
