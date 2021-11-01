344685719238f106a8fe2e3aa3376de982433b79
Mikaela Hedberg <mie@du.se>
2021-11-01 17:39:03 +0100

    clean up

---
+++ js/api.js
@@ -0,0 +1,22 @@
+/* Api.js
+  Code for general CRUD-operations with promises.
+  Generally written (not necesarily users)
+*/
+const url = 'https://rest-small.herokuapp.com/users';
+
+//get object by ID
+function get(id) {
+  return fetch(`${url}/${id}`);
+}
+
+//Get All, converted to JSON for cleanest possible result to work with
+function getAllJSON() {
+  //first response returns data stream, needs to be converted to json.
+  const result = fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    });
+
+  return result;
+}
