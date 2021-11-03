9cd12174cbfdfe53f0fad42f5967350c87f61c06
Mikaela Hedberg <mie@du.se>
2021-11-03 12:18:29 +0100

    checkin to match video

--- js/User.js
+++ js/User.js
@@ -4,12 +4,10 @@
 */
 
 function renderUser(user) {
-  let presentation = user.presentation;
-  if (presentation.length > 85)
-    presentation = presentation.substring(0, 85).concat('...');
   return `
   <li class="card card-catalina-blue text-dark">
-      <a class="card-link card-link-catalina-blue" data-user="${user.id}" href="./update.html"><span title="Uppdatera ${user.f_name} ${user.l_name}">Uppdatera</span></a>
+      <a class="card-link card-link-catalina-blue" href="./update.html?id=${user.id}">
+      <span title="Uppdatera ${user.f_name} ${user.l_name}">Uppdatera</span></a>
       <span class="card-border card-border-left card-border-left-catalina-blue"></span>              
       <span class="card-border card-border-top card-border-top-catalina-blue"></span>
       <span class="card-border card-border-right card-border-right-catalina-blue"></span>
@@ -22,7 +20,7 @@ function renderUser(user) {
       </div>
       <div class="card-body">
           <h5 class="card-title card-title-razzmatazz">${user.f_name} ${user.l_name}</h5>
-          <p class="card-text">${presentation}</p>
+          <p class="card-text">${user.presentation}</p>
       </div>
       <p class="card-text card-email card-email-persian-green"><a href="mailto:${user.email}" title="Mejla ${user.f_name} ${user.l_name}">${user.email}</a></p>
   </li>
